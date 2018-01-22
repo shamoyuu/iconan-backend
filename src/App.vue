713b82e10f8fffb0f823a0a666be9746d4330f97
MELEONG-PC\meleong <shamoyuu@qq.com>
2018-01-22 23:36:08 +0800

    添加Element-UI

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
-    <div id="app">
-        <img src="./assets/logo.png">
-        <router-view/>
-    </div>
+    <el-container class="app">
+        <el-header height="60px">Header</el-header>
+        <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-main>
+                <router-view/>
+            </el-main>
+        </el-container>
+        <el-footer height="40px">爱柯南后台系统 - 静茹♂鱼</el-footer>
+    </el-container>
 </template>
 
 <script>
@@ -12,12 +18,31 @@
 </script>
 
 <style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
+    @import "./assets/less/main.less";
+
+    .app {
+        height: 100%;
+    }
+    
+    .el-header, .el-footer {
+        background-color: #B3C0D1;
+        color: #333;
         text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
+    }
+
+    .el-footer {
+        line-height: 40px;
+    }
+
+    .el-aside {
+        background-color: #D3DCE6;
+        color: #333;
+        text-align: center;
+    }
+
+    .el-main {
+        background-color: #E9EEF3;
+        color: #333;
+        text-align: center;
     }
 </style>
